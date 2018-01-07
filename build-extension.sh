e295ceb3e91052f6661de46ed22d8d1740b5a9ad
Alexander Shchapov <sasha@imedicare.com>
2018-01-07 21:32:36 +0200

    Update build script.

--- build-extension.sh
+++ build-extension.sh
@@ -5,7 +5,13 @@ rollup -i js/index.js -f iife -o build/index-bundle.js
 rollup -i js/listener.js -f iife -o build/listener-bundle.js
 popd
 
-rm -rfv attention-monster.zip
+rm -rfv *.zip
 pushd extension
-zip -9 -v -r ../attention-monster.zip *
+VERSION=`cat manifest.json | grep version | grep -v manifest | awk '{split($0,a,":"); print a[2]}' | sed s/\"//g | sed s/\,//g | sed s/\ //g`
+VERSIONED_FILENAME="attention-monster-$VERSION.zip"
+zip -9 -v -r "../$VERSIONED_FILENAME" *
 popd
+
+echo "**********************************************"
+echo "Build complete: $VERSIONED_FILENAME"
+echo "**********************************************"
